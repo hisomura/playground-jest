bcc64519268b26bda66b9cc5a28bbd4fa72f2a15
hisomura <hisomura.noreply@gmail.com>
2020-08-02 06:41:17 +0900

    公式ページからサンプルコピペ

---
+++ src/sound-player/sound-player.ts
@@ -0,0 +1,12 @@
+// https://jestjs.io/docs/ja/es6-class-mocks
+
+export default class SoundPlayer {
+  protected foo: string
+  constructor() {
+    this.foo = 'bar';
+  }
+
+  playSoundFile(fileName) {
+    console.log('Playing sound file ' + fileName);
+  }
+}
